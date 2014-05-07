59a853fc8f1335c03acefab7bcb76d4409a27eb1
Klederson Bueno <klederson@klederson.com>
2014-05-07 16:00:38 -0300

    First Commit

---
+++ src/angular-i18n-service.js
@@ -0,0 +1,111 @@
+'use strict';
+
+angular.module('translate',[]).factory('translate', function ($rootScope, $http, $q) {
+
+    //Global Service
+    var service = this;
+    var $scope = $rootScope.$new(true);
+
+    service.parser = null;
+
+    $scope.settings = {
+        defaults : {
+            locale : "en"
+        },
+        locales : []
+    };
+
+    $scope.locale = {
+        code : null,
+        data : {},
+        avaliable : {}
+    }
+
+    //Set $scope.locale.data to the current locale
+    $scope.$watch('locale.code',function(n,o) {
+        if(typeof $scope.locale.avaliable[n] !== "undefined") {
+            $scope.locale.data = $scope.locale.avaliable[n];
+        } else if(o !== null || n !== null) {
+            console.error ( "This locale has no data to translate" );
+        }
+    });
+
+    $scope.get = function() {
+        return $scope.locale.data;
+    }
+
+    //Set or Get the current locale data
+    $scope.current = function(code) {
+        if(typeof code === "undefined" || code.trim() === "") {
+            return $scope.locale.code;
+        } else {
+            $scope.locale.code = code;
+        }
+    }
+
+    $scope.add = function(code,path) {
+        return service.loadFile(path).success(function(r) {
+            try {
+                $scope.locale.avaliable[code] = service.parse(code,r);
+                if(code === $scope.settings.defaults.locale) {
+                    $scope.current($scope.settings.defaults.locale);
+                }
+                $rootScope.$broadcast('i18n.newTranslation',{
+                    code : code
+                });
+            } catch(error) {
+                console.error(error);
+            }
+        });
+    }
+
+    service.parse = function(code,data) {
+        //detect type
+        try {
+            var parsed = JSON.parse(data);
+        } catch(err) {
+            try {
+                //TODO: I know it' uggly ... lets fix this someday!(?)
+                //parse YAML
+                data = jsyaml.load(data);
+            } catch(err2) {
+                throw "Unable to parse the content on " + code;
+            }
+        }
+
+        return data;
+    }
+
+    $scope.start = function(settings) {
+        $scope.settings = angular.extend($scope.settings,settings);
+
+        var def = $q.defer();
+        var total = 0;
+        for(i in $scope.settings.locales) {
+            total++;
+        }
+
+        var i = 0;
+        for(var code in $scope.settings.locales) {
+            try {
+                $scope.add(code,$scope.settings.locales[code]).success(function() {
+                    i++;
+                    if(i == total) {
+                        def.resolve($scope);
+                    }
+                });
+            } catch(error) {
+                i++;
+                console.error(error);
+            }
+        }
+
+        return def.promise;
+    }
+
+    service.loadFile = function(path) {
+        return $http.get(path);
+    }
+
+    return $scope;
+});
